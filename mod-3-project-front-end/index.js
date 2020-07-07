803a33fd552175a727c88513d7db64f3754da707
skandow <kando1sr@gmail.com>
2020-07-07 12:08:48 -0500

    morning work

--- mod-3-project-front-end/index.js
+++ mod-3-project-front-end/index.js
@@ -1,12 +1,29 @@
 let displayForm = false;
 
-document.addEventListener("DOMContentLoaded", renderForm)
+document.addEventListener("DOMContentLoaded", renderPage)
 
-function renderForm() {
+function renderPage() {
+    fetchEvents();
     toggleForm();
     const form = document.getElementById("daily-log-form")
-    console.log(form)
     form.addEventListener("submit", submitEvent)
+}
+
+function fetchEvents() {
+    fetch("http://localhost:3000/events")
+    .then(resp => resp.json())
+    .then(events => renderEvents(events))
+}
+
+function renderEvents(events) {
+    console.log(events.data)
+    events.data.forEach(event => {
+        renderEvent(event)
+    })
+    const deleteButtons = document.getElementsByClassName("delete")
+    for (let i=0; i<deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", deleteEvent)
+    }
 }
 
 function toggleForm() {
@@ -41,12 +58,22 @@ function submitEvent(event) {
     }
     fetch("http://localhost:3000/events", reqObj)
     .then(resp => resp.json())
-    .then(data => renderEvent(data))
+    .then(data => renderEvent(data.data))
 }
 
 function renderEvent(data) {
     const eventList = document.getElementById("today-events")
-    eventList.innerHTML += `<li>Event: ${data.data.attributes.content}<br>
-    Emotion: ${data.data.attributes.emotion}</li>`
+    eventList.innerHTML += `<li id=${data.id}><hr style="width:25%;margin-left:0">Event: ${data.attributes.content}<br>
+    Emotion: ${data.attributes.emotion}<br>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</li><br<br><br>`
+}
+
+function deleteEvent(event) {
+    const id = event.target.parentNode.id 
+    const url = "http://localhost:3000/events" + "/" + id 
+    fetch(url, {method: "DELETE"})
+    .then(resp => resp.json())
+    .then(json => event.target.parentNode.remove())
 }
 
